31ac154457efd1222282e72b7740d1570ca7d0f5
Şeymanur Diktaş <seymanurdiktas@gmail.com>
2021-01-23 18:52:53 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T, X>
+    {
+        T[] items;
+        T[] tempItems;
+        X[] valueItems;
+        X[] tempValue;
+
+        public MyDictionary()
+        {
+            items = new T[0];
+            valueItems = new X[0];
+        }
+
+        public void Add(T item, X value)
+        {
+            tempItems = items;
+            tempValue = valueItems;
+            items = new T[items.Length + 1];
+            valueItems = new X[valueItems.Length + 1];
+            for (int i = 0; i < tempItems.Length; i++)
+            {
+                items[i] = tempItems[i];
+                tempValue[i] = valueItems[i];
+            }
+            items[items.Length - 1] = item;
+            valueItems[valueItems.Length - 1] = value;
+
+        }
+        public T GetT(X value)
+        {
+            int index = Array.IndexOf(valueItems, value);
+            return items[index];
+        }
+        public X GetX(T item)
+        {
+            int index = Array.IndexOf(items, item);
+            return valueItems[index];
+        }
+        public int Length()
+        {
+            return items.Length;
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> array = new MyDictionary<int, string>();
+            array.Add(1, "Ayse");
+            array.Add(2, "Fatma");
+
+        }
+    }
+}
